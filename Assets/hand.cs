4a095066173845892c792da581359768ede93c00
rohit563 <rohit-banda@uiowa.edu>
2016-11-05 08:18:30 -0500

    Test

---
+++ Assets/hand.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hand : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter(Collision col) {
+		Debug.Log("Enter called..");
+	}
+	void OnCollisionStay(Collision col) {
+		Debug.Log ("Stay occuring..");
+	}
+	void onCollisionExit(Collision collision) {
+		Debug.Log ("Exit called..");
+	}
+}
